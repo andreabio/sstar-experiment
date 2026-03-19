7c6f7188371a39aebb62540dc80b3081c1e63fe5
andreabio <andreakoca123@gmail.com>
2026-03-19 15:25:34 +0100

    Update SLURM submit script and environment configuration for S* workflow

--- submit.sh
+++ submit.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=sstar-qr-analysis
+#SBATCH --job-name=sstar-experiment
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=16gb
-#SBATCH --time=24:00:000
+#SBATCH --mem=16G
+#SBATCH --time=24:00:00
 #SBATCH --no-requeue
 
+source ~/.bashrc
+conda activate sstar-experiment
 
-snakemake -c 1 --profile config/slurm --rerun-incomplete --rerun-triggers mtime
+snakemake -s workflow/Snakefile \
+  --cores 8 \
+  --use-conda \
+  --rerun-incomplete \
+  --rerun-triggers mtime
